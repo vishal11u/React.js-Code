c8a76d846c831db4a7deed180bdcd190b0e8288c
Vishal Shitole <136974299+vishal11u@users.noreply.github.com>
2024-02-21 16:59:12 +0530

    Update EditPostData.js
    change

--- src/ApiCall/EditPostData.js
+++ src/ApiCall/EditPostData.js
@@ -1,39 +1,60 @@
-import React from 'react'
-import axios from 'axios'
+import React from 'react';
+import axios from 'axios';
 
-function Postdata() {
-    const  postObj={
-        "name":"",
-        "username":""
-        }
+function PostData() {
+  const [data, setData] = React.useState({
+    departmentCode: '',
+    nameofDepartment: '',
+    noofStudent: '',
+  });
+  const [post, setPost] = React.useState(null);
+  const [error, setError] = React.useState('');
 
-    const[data,setdata] = React.useState(postObj)
-    const [post, setpost] = React.useState(null)
- 
-  const senddata=()=>{
-    axios.post("https://jsonplaceholder.typicode.com/users",data)
-    .then((Response)=>Response.data)
-    .then(res=>{
-          setpost(res)
-        console.log(res)
-    })
-  }
+  const sendData = async () => {
+    try {
+      const response = await axios.post("http://:5001/api/collegee/savedepartment", data);
+      setPost(response.data);
+      setError('');
+    } catch (err) {
+      setError('Failed to send data. Error: ' + err.message);
+      console.error(err);
+    }
+  };
 
-  const Handlechange=(event)=>{   
-   setdata({...data,[event.target.name]:event.target.value})
-  
-}
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setData(prevData => ({ ...prevData, [name]: value }));
+  };
+
   return (
-    <div style={{textAlign:"center"}}>
-        <h1>Post Data Using Axois</h1>
-        <input type="text" name='name' placeholder='Name' onChange={Handlechange}/><br />
-        <input type="text"name='username' placeholder='username'onChange={Handlechange} /><br />
-        <button type='button' onClick={senddata}>Send Data</button>
-
-        <p>Generated id:{post ?post.id:""}</p>
-      
+    <div style={{ textAlign: "center" }}>
+      <h1>Post Data Using Axios</h1>
+      <input
+        type="number"
+        name="departmentCode"
+        placeholder="Department Code"
+        value={data.departmentCode}
+        onChange={handleChange}
+      /><br />
+      <input
+        type="text"
+        name="nameofDepartment"
+        placeholder="Name of Department"
+        value={data.nameofDepartment}
+        onChange={handleChange}
+      /><br />
+      <input
+        type="number"
+        name="noofStudent"
+        placeholder="Number of Students"
+        value={data.noofStudent}
+        onChange={handleChange}
+      /><br />
+      <button type="button" onClick={sendData}>Send Data</button>
+      <p>{post ? JSON.stringify(post) : ''}</p>
+      <p>{error}</p>
     </div>
-  )
+  );
 }
 
-export default Postdata;
+export default PostData;
