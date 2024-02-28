bacae3cf6601bda5e3b894dfccf8513330541ca4
Vishal Shitole <136974299+vishal11u@users.noreply.github.com>
2024-02-28 08:34:40 +0530

    Update ApiCRUD.js
    change

--- src/Api/ApiCRUD.js
+++ src/Api/ApiCRUD.js
@@ -1,47 +1,35 @@
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import React, { useEffect, useState } from 'react';
 import TextField from '@mui/material/TextField';
-import { MdDelete } from "react-icons/md";
+import { MdDelete, MdEdit } from "react-icons/md";
 import axios from 'axios';
 import { toast, Bounce } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { RxCross2 } from "react-icons/rx";
+import Tooltip from '@mui/material/Tooltip';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 
-export default function ApiCRUD({ users, setUsers }) {
-    console.log('data is' + users);
+function ApiCRUD({ table, setTable, users, setUsers, handleEdit }) {
+    
+    const getData = "http://192.168.0.115:5001/api/saveFront/getallfrontendfrom";
+
+    useEffect(() => {
+        handleData();
+    }, []);
 
     const handleData = () => {
-        axios.get("http://192.168.0.115:5001/api/saveFront/getFrontendFrom")
+        axios.get(getData)
             .then((response) => {
-                console.log(response.data.result);
-                let getData = (response.data.result)
-                setUsers(getData)
+                setUsers(response.data.result);
             })
             .catch((error) => {
                 console.log("error is" + error);
             })
-    }
+    };
 
-    const handleShow = () => {
-        axios.get("http://192.168.0.115:5001/api/saveFront/getFrontendFrom")
-            .then((response) => {
-                console.log(response.data.result);
-                setUsers(response.data.result)
-            })
-            .catch((error) => {
-                console.log("error is" + error);
-            })
-    }
-
-    const dataDelete = (i, id) => {
+    const dataDelete = (id, i) => {
         axios.delete(`http://192.168.0.115:5001/api/saveFront/deletedata/${id}`)
             .then((response) => {
-                console.log(response.data.result);
+                console.log(response.data);
                 if (i !== null) {
                     let newData = [...users];
                     newData.splice(i, 1)
@@ -75,67 +63,54 @@ export default function ApiCRUD({ users, setUsers }) {
             })
     };
 
-    const handleDelete = (i) => {
-        let remove = ([...users])
-        remove.splice(i, 1)
-        setUsers(remove)
-    }
-
-    React.useEffect(() => {
-        handleData();
-        handleShow();
-    }, [])
-
     const handleSearch = (searchQuery) => {
-        if (searchQuery.trim() !== '') {
+        if (searchQuery !== null) {
             const res = users.filter((user) =>
-                user.firstname.toLowerCase().includes(searchQuery.toLowerCase()) ||
-                user.mobileno.includes(searchQuery)
+                user.firstname.toLowerCase().includes(searchQuery.toLowerCase())
             );
             setUsers(res);
         } else {
-            setUsers([...users]);
+            handleData();
         }
     };
 
-
     return (
-        <TableContainer component={Paper}>
-            <TextField size='small' className='float-right' sx={{ margin: '7px 12px' }} label='Enter Name' variant='outlined' placeholder='Enter Name' onChange={(e) => handleSearch(e.target.value)} />
-            <Table sx={{ color: 'white' }} aria-label="simple table">
-                <TableHead sx={{ backgroundColor: '#242424', }}>
-                    <TableRow sx={{ color: 'white' }}>
-                        <TableCell sx={{ color: 'white' }}>Firstname</TableCell>
-                        <TableCell sx={{ color: 'white' }}>Lastname</TableCell>
-                        <TableCell sx={{ color: 'white' }}>Age</TableCell>
-                        <TableCell sx={{ color: 'white' }}>Mobile</TableCell>
-                        <TableCell sx={{ color: 'white' }}>Email</TableCell>
-                        <TableCell sx={{ color: 'white' }}>Address</TableCell>
-                        <TableCell sx={{ color: 'white' }}>Aadhar Card</TableCell>
-                        <TableCell sx={{ color: 'white' }}>Pincode</TableCell>
-                        <TableCell sx={{ color: 'white' }}>Blood Group</TableCell>
-                        <TableCell sx={{ color: 'white' }}>Occupation</TableCell>
-                        <TableCell sx={{ color: 'white' }}>Action</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {users.map((row, i) => (
-                        <TableRow key={i.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }} >
-                            <TableCell> {row.firstname}</TableCell>
-                            <TableCell>{row.lastname}</TableCell>
-                            <TableCell>{row.gmail}</TableCell>
-                            <TableCell>{row.mobileno}</TableCell>
-                            <TableCell>{row.age}</TableCell>
-                            <TableCell> {row.address}</TableCell>
-                            <TableCell>{row.aadharaadharcardno}</TableCell>
-                            <TableCell>{row.pincode}</TableCell>
-                            <TableCell>{row.bloodGroup}</TableCell>
-                            <TableCell>{row.ocupation}</TableCell>
-                            <TableCell className='cursor-pointer' onClick={() => dataDelete(row.id, i)}><MdDelete size={23} /></TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
+        <TableContainer component={Paper} sx={{ width: '90%', margin: 'auto ', marginBottom: '30px' }}>
+            {table && (
+                <>
+                    <div className='flex justify-between w-full items-center p-2.5'>
+                        <Tooltip title="close" arrow placement='top'>
+                            <button type='button' className='text-red-600 border-[2px] h-6 border-red-600 float-right' onClick={() => setTable(false)}><RxCross2 size={20} /> </button>
+                        </Tooltip>
+                        <TextField size='small' className='float-right' sx={{ width: '17%' }} label='Search by Name/Mobileno.' variant='outlined' placeholder='Search by Name/Mobileno.' onChange={(e) => handleSearch(e.target.value)} />
+                    </div>
+                    <Table sx={{ color: 'white' }} aria-label="simple table">
+                        <TableHead sx={{ backgroundColor: '#242424', }}>
+                            <TableRow sx={{ color: 'white' }}>
+                                {Object.keys(users[0]).map((key) => (
+                                    <TableCell key={key}>{key}</TableCell>
+                                ))}
+                                <TableCell>Action</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {users.map((row, i) => (
+                                <TableRow key={i} sx={{ '&:last-child td, &:last-child th': { border: 0 } }} >
+                                    {Object.values(row).map((value, j) => (
+                                        <TableCell key={j}>{value}</TableCell>
+                                    ))}
+                                    <TableCell>
+                                        <MdEdit className='cursor-pointer' size={23} onClick={() => handleEdit(row)} />
+                                        <MdDelete className='cursor-pointer' size={23} onClick={() => dataDelete(row.Id, i)} />
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </>
+            )}
         </TableContainer>
     );
 }
+
+export default React.memo(ApiCRUD);
