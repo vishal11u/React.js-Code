fc40c9e7501550abcf975cf820ff3338a162a8f9
Vishal Shitole <136974299+vishal11u@users.noreply.github.com>
2024-02-28 08:35:03 +0530

    Update UserForm.js
    change

--- src/Api/UserForm.js
+++ src/Api/UserForm.js
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState} from 'react';
 import TextField from '@mui/material/TextField';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import axios from 'axios';
-import ApiCRUD from './ApiCRUD'
+import ApiCRUD from './ApiCRUD';
 import { toast, Bounce } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { FcRules } from "react-icons/fc";
+import Tooltip from '@mui/material/Tooltip';
 
 const schema = yup.object({
     firstname: yup.string().required(),
@@ -18,7 +20,7 @@ const schema = yup.object({
     aadharaadharcardno: yup.number().required(),
     pincode: yup.number().required(),
     bloodGroup: yup.string().required(),
-    ocupation: yup.string().required(),
+    occupation: yup.string().required(),
 }).required();
 
 function UserForm() {
@@ -27,10 +29,20 @@ function UserForm() {
     });
 
     const [users, setUsers] = useState([]);
+    const [table, setTable] = useState(false);
+    const [selectedUser, setSelectedUser] = useState(null);
+    const [isUpdateMode, setIsUpdateMode] = useState(false);
 
     const onSubmit = (data, e) => {
         e.preventDefault();
-        console.log("data", data);
+        if (isUpdateMode) {
+            handleUpdate(data);
+        } else {
+            handleCreate(data);
+        }
+    };
+
+    const handleCreate = (data) => {
         axios.post("http://192.168.0.115:5001/api/saveFront/frontfrom", data)
             .then((response) => {
                 console.log(response);
@@ -63,10 +75,59 @@ function UserForm() {
             })
     };
 
+    const handleUpdate = (data) => {
+        axios.put(`http://192.168.0.115:5001/api/saveFront/updateData/${selectedUser.id}`, data)
+            .then((response) => {
+                console.log(response);
+                const updatedUsers = users.map(user => {
+                    if (user.id === selectedUser.id) {
+                        return { ...user, ...data };
+                    } else {
+                        return user;
+                    }
+                });
+                setUsers(updatedUsers);
+                setSelectedUser(null);
+                setIsUpdateMode(false);
+                reset();
+                toast.success('User updated', {
+                    position: "top-right",
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "dark",
+                    transition: Bounce,
+                });
+            })
+            .catch((error) => {
+                console.log("Error" + error);
+                toast.error('Error', {
+                    position: "top-right",
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "dark",
+                    transition: Bounce,
+                });
+            });
+    };
+
+    const handleEdit = (user) => {
+        setSelectedUser(user);
+        setIsUpdateMode(true);
+        reset(user);
+    };
 
     return (
-        <div className='p-4 h-[100%]'>
-            <form onSubmit={handleSubmit(onSubmit)} className='mb-4 border shadow-lg rounded mx-auto px-10 py-6 md:w-[34vw] space-y-3'>
+        <div className='h-full w-full'>
+            <form onSubmit={handleSubmit(onSubmit)} className='m-4 border shadow-lg mx-auto rounded pb-5 pt-2 w-full md:w-[70%] lg:w-[32%] space-y-3'>
+                <img className='h-32 mx-auto relative ' src='https://img.freepik.com/free-vector/family-doctor-online-service-platform-healthcare-modern-medicine-treatment-expertize-diagnostic-online-appointment-flat-vector-illustration_613284-3026.jpg?t=st=1708665096~exp=1708668696~hmac=bc9b87ce3cc5aae34ccf15402861670cf04aeaad8996f2fbb7d793c0add452c5&w=996' alt='' />
                 <div className='flex items-center justify-center space-x-3'>
                     <TextField
                         size='small'
@@ -97,7 +158,6 @@ function UserForm() {
                     <TextField
                         size='small'
                         label="Mobile No"
-                        type='number'
                         placeholder='Mobile No'
                         variant="outlined"
                         {...register("mobileno")}
@@ -108,7 +168,6 @@ function UserForm() {
                     <TextField
                         size='small'
                         label="Age"
-                        type='number'
                         placeholder='Age'
                         variant="outlined"
                         {...register("age")}
@@ -116,8 +175,8 @@ function UserForm() {
                     />
                     <TextField
                         size='small'
-                        label="Aadhar No"
-                        placeholder='Aadhar No'
+                        label="Aadhar Card No"
+                        placeholder='Aadhar Card No'
                         variant="outlined"
                         {...register("aadharaadharcardno")}
                         error={!!errors.aadharaadharcardno}
@@ -135,7 +194,6 @@ function UserForm() {
                     <TextField
                         size='small'
                         label="Pincode"
-                        type='number'
                         placeholder='Pincode'
                         variant="outlined"
                         {...register("pincode")}
@@ -154,16 +212,28 @@ function UserForm() {
                     <TextField
                         size='small'
                         label="Occupation"
-                        placeholder='Ocupation'
+                        placeholder='Occupation'
                         variant="outlined"
-                        {...register("ocupation")}
-                        error={!!errors.ocupation}
+                        {...register("occupation")}
+                        error={!!errors.occupation}
                     />
                 </div>
-                <button type='submit' className='py-1 px-5 mt-2 w-full bg-green-600 text-white rounded-md shadow-lg border-none transition-all hover:bg-green-700'>Submit</button>
+                <div className='flex justify-center w-full items-center'>
+                    <button type='submit' className='py-2 md:w-[50%] px-5 bg-green-600 text-white rounded-md shadow-lg border-none transition-all hover:bg-green-700'>
+                        {isUpdateMode ? 'Update' : 'Submit'}
+                    </button>
+                    {users.length > 0 && (
+                        <Tooltip title="Show Table" arrow>
+                            <button className="drop-shadow-lg" type='button' onClick={() => setTable(true)}>
+                                <FcRules size={30} />
+                            </button>
+                        </Tooltip>
+                    )}
+                </div>
+
             </form>
 
-            <ApiCRUD users={users} setUsers={setUsers} />
+            <ApiCRUD users={users} setUsers={setUsers} table={table} setTable={setTable} handleEdit={handleEdit} />
         </div>
     );
 }
